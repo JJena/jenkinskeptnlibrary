a9699d7a3dc0bc94f8f737589bc57feeff8fe365
JJena <jadumani2003@gmail.com>
2023-06-21 15:47:03 -0400

    Update keptnTaskHandler.java

--- src/main/java/com/test/keptn/keptnTaskHandler.java
+++ src/main/java/com/test/keptn/keptnTaskHandler.java
@@ -1,30 +1,39 @@
 package com.test.keptn;
 
 
-import java.io.UnsupportedEncodingException;
-
 public class keptnTaskHandler {
 
     private static final String keptnEndpoint = "http://10.7.12.37:8081";
     private static String xToken = "";
     private static String testMode = "";
+    private static String project = "";
+    private static String service = "";
+    private static String stage = "";
+    private static String sloFile = "";
+    private static String evaluationDurationMinutes = "";
 
-    public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
+    public static void main(String[] args) throws InterruptedException {
 
         if (args.length > 0)
         {
             xToken = args[0];
             testMode = args[1];
+            project = args[2];
+            service = args[3];
+            stage = args[4];
+            sloFile = args[5];
+            evaluationDurationMinutes = args[6];
         }
     switch (testMode){
         case "Test_And_Evaluate":
-            String keptnContext = runTest.runTest(keptnEndpoint,xToken);
+            String keptnContext = runTest.runTest(keptnEndpoint,xToken,project,service,stage);
             Thread.sleep(3000);
             runTest.checkTestCompletion(keptnEndpoint,xToken,keptnContext);
+            Thread.sleep(3000);
+            runEvaluate.evaluateTest(keptnEndpoint,xToken,project,service,stage,sloFile,evaluationDurationMinutes);
             break;
         case "Evaluate":
-            runEvaluate evaluateTest = new runEvaluate();
-            evaluateTest.runEvaluate(keptnEndpoint,xToken);
+            runEvaluate.evaluateTest(keptnEndpoint,xToken,project,service,stage,sloFile,evaluationDurationMinutes);
             break;
         default:
             throw new RuntimeException("Test Mode not supported: "+testMode);
